d8bed69bbb9f8157f5449f5be776d80bfc36274d
ingridmarcelasantosibarra <marcesntos@gmail.com>
2025-03-01 10:54:19 -0500

    add libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.com.android.tools.build.gradle)
+        classpath(libs.com.google.gms.google.services)
+        classpath(libs.org.jetbrains.kotlin.gradle.plugin)
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
 }
